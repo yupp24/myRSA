27f641d82cf87a446c2afc1891de61d4cae79d4d
JP <JP@DESKTOP-6FBMIM7>
2022-05-19 13:33:06 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace myRSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnClear2 = new System.Windows.Forms.Button();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.txtCypherText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(93, 72);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(89, 34);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(460, 72);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 34);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(571, 72);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(89, 34);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnClear2
+            // 
+            this.btnClear2.Location = new System.Drawing.Point(938, 72);
+            this.btnClear2.Name = "btnClear2";
+            this.btnClear2.Size = new System.Drawing.Size(75, 34);
+            this.btnClear2.TabIndex = 3;
+            this.btnClear2.Text = "Clear";
+            this.btnClear2.UseVisualStyleBackColor = true;
+            this.btnClear2.Click += new System.EventHandler(this.btnClear2_Click);
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(93, 158);
+            this.txtPlainText.Multiline = true;
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(442, 483);
+            this.txtPlainText.TabIndex = 4;
+            // 
+            // txtCypherText
+            // 
+            this.txtCypherText.Location = new System.Drawing.Point(571, 158);
+            this.txtCypherText.Multiline = true;
+            this.txtCypherText.Name = "txtCypherText";
+            this.txtCypherText.Size = new System.Drawing.Size(442, 483);
+            this.txtCypherText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 653);
+            this.Controls.Add(this.txtCypherText);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.btnClear2);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
+            this.Text = "Public Key Encyrption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnClear2;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.TextBox txtCypherText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace myRSA
+{
+    public partial class Form1 : Form
+    {
+        String publicKey, privateKey; //strings to hold public and private keys
+        UnicodeEncoding encoder = new UnicodeEncoding();
+        public Form1()
+        {
+            RSACryptoServiceProvider myRSA = new RSACryptoServiceProvider();
+            InitializeComponent();
+            privateKey = myRSA.ToXmlString(true);
+            publicKey = myRSA.ToXmlString(false);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPlainText.Text = "";
+            txtPlainText.Refresh();   
+        }
+
+        private void btnClear2_Click(object sender, EventArgs e)
+        {
+            txtCypherText.Text = "";
+            txtPlainText.Refresh();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            var myRSA = new RSACryptoServiceProvider();
+            //split data into array
+            var dataArray = txtCypherText.Text.Split(new char[] { ',' });
+            //convert to bytes
+            byte[] dataByte = new byte[dataArray.Length];
+            for (int i = 0; i < dataArray.Length; i++) dataByte[i] = Convert.ToByte(dataArray[i]);
+            //decrypt data array
+            myRSA.FromXmlString(privateKey);
+            var decryptedBytes = myRSA.Decrypt(dataByte, false);
+            //place in text box
+            txtPlainText.Text = encoder.GetString(decryptedBytes);
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            var myRSA = new RSACryptoServiceProvider();
+            //sets up crypto service with proper key
+            myRSA.FromXmlString(publicKey);
+            //encode data to encrypt as a byte array
+            var dataToEncrypt = encoder.GetBytes(txtPlainText.Text);
+            //encrypt byte array
+            var encryptedByteArray = myRSA.Encrypt(dataToEncrypt, false).ToArray();
+            var length = encryptedByteArray.Count();
+            var item = 0;
+            var sb = new StringBuilder();
+            //change each byte in the encrypted byte array to text
+            foreach (var x in encryptedByteArray)
+            {
+                item++;
+                sb.Append(x);
+                if (item < length) sb.Append(",");
+            }
+            txtCypherText.Text = sb.ToString();
+
+        }
+    }
+}
